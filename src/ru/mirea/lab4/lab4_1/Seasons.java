f2d4325fcad3c0caa7d2129ca6ffcbb1753ef54d
pervanne69 <samvel.pogosyan2006@gmail.com>
2024-09-13 21:50:28 +0300

    lab4_1

--- src/ru/mirea/lab4/lab4_1/Seasons.java
+++ src/ru/mirea/lab4/lab4_1/Seasons.java
@@ -3,18 +3,31 @@ package ru.mirea.lab4.lab4_1;
 
 public class Seasons {
     public enum Season {
-        January(-5.8),
-        February(-6.1),
-        March(-0.5),
-        April(5.5),
-        May(14.7),
-        June(16f),
-        July(20f);
-        August(),
-        September,
-        October,
-        November,
-        December;
+        WINTER(3.6) {
+            @Override
+            public void getDescription() {
+                System.out.println("Холодное время года");
+            }
+        },
+        SPRING(14f) {
+            @Override
+            public void getDescription() {
+                System.out.println("Цветущее время года");
+            }
+        },
+        SUMMER(17f) {
+            @Override
+            public void getDescription() {
+                System.out.println("Теплое время года");
+            }
+        },
+        AUTUMN(11.1) {
+            @Override
+            public void getDescription() {
+                System.out.println("Прохладное время года");
+            }
+        };
+
 
         private final double temp;
 
@@ -22,32 +35,42 @@ public class Seasons {
             this.temp = temp;
         }
 
+        public double getTemp() {
+            return temp;
+        }
+
         private int getMonthIndex() {
             return this.ordinal() + 1;
         }
+
+        public abstract void getDescription();
     }
 
     public static void checkLovelySeason(Season season) {
         switch (season) {
-            case June, July, August:
+            case SUMMER:
                 System.out.println("Я люблю лето!");
                 break;
-            case January, February, December:
+            case WINTER:
                 System.out.println("Я люблю зиму!");
                 break;
-            case September, October, November:
+            case AUTUMN:
                 System.out.println("Я не люблю осень!");
                 break;
-            case March, April, May:
+            case SPRING:
                 System.out.println("Я немножко люблю весну!");
                 break;
         }
     }
 
+
     public static void main(String[] args) {
-        Season jan = Season.January;
-        Season apr = Season.April;
-        System.out.println(jan.getMonthIndex());
-        checkLovelySeason(apr);
+        Season winter = Season.WINTER;
+        checkLovelySeason(winter);
+
+        for (Season season: Season.values()) {
+            System.out.printf("%s: Средняя температура - %.2f, Описание времени года - ", season, season.getTemp());
+            season.getDescription();
+        }
     }
 }
