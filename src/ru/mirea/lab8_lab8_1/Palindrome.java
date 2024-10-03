9613c3bc6ea81a7b6023ac3d9f448ea35fb8354f
pervanne69 <samvel.pogosyan2006@gmail.com>
2024-10-03 11:21:51 +0300

    lab8_1

---
+++ src/ru/mirea/lab8_lab8_1/Palindrome.java
@@ -0,0 +1,29 @@
+package ru.mirea.lab8_lab8_1;
+
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static boolean isPalindrome(String word, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (word.charAt(start) != word.charAt(end)) {
+            return false;
+        }
+        return isPalindrome(word, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        String word;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите слово: ");
+        word = sc.nextLine();
+        if (isPalindrome(word, 0, word.length() - 1)) {
+            System.out.println("Слово является палиндромом");
+        } else {
+            System.out.println("Слово не является палиндромом");
+        }
+    }
+}
+
