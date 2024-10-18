c4f5c607c85dd8fe88c2c2558fdfd07d6774c5d6
pervanne69 <samvel.pogosyan2006@gmail.com>
2024-10-18 18:37:35 +0300

    lab14_5

---
+++ src/ru/mirea/lab14/lab14_5/CheckPlus.java
@@ -0,0 +1,25 @@
+package ru.mirea.lab14.lab14_5;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class CheckPlus {
+
+    public static boolean containsDigitsWithoutPlus(Pattern pattern, String string) {
+        Matcher matcher = pattern.matcher(string);
+        while (matcher.find()) {
+            System.out.println("Найдена цифра без знака «+»: " + matcher.group());
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String string1 = "(1 + 8) - 9 / 4";
+        String string2 = "6 / 5 - 2 * 9";
+        String regex = "\\d(?!\\s*\\+)";
+        Pattern pattern = Pattern.compile(regex);
+
+        System.out.println("Проверка текста 1: " + containsDigitsWithoutPlus(pattern, string1));
+        System.out.println("Проверка текста 2: " + containsDigitsWithoutPlus(pattern, string2));
+    }
+}
