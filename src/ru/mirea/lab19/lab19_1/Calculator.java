f4172c9ceb9ff0d8b7c1587fbc5860449b4b217e
pervanne69 <samvel.pogosyan2006@gmail.com>
2024-10-24 09:50:12 +0300

    lab15

---
+++ src/ru/mirea/lab19/lab19_1/Calculator.java
@@ -0,0 +1,86 @@
+package ru.mirea.lab19.lab19_1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Calculator {
+    private JFrame frame;
+    private JTextField textField;
+    private String operator;
+    private double num1, num2, result;
+
+    public Calculator() {
+        frame = new JFrame("Calculator");
+        textField = new JTextField();
+        operator = "";
+        num1 = num2 = result = 0;
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+
+        String[] buttons = {
+                "7", "8", "9", "/",
+                "4", "5", "6", "*",
+                "1", "2", "3", "-",
+                "0", ".", "=", "+"
+        };
+
+        for (String btnText: buttons) {
+            JButton button = new JButton(btnText);
+            button.addActionListener(new ButtonClickListener());
+            panel.add(button);
+        }
+
+        textField.setHorizontalAlignment(SwingConstants.RIGHT);
+        textField.setEditable(false);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(textField, BorderLayout.NORTH);
+        frame.add(panel, BorderLayout.CENTER);
+
+        frame.setSize(300, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    private class ButtonClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+
+            if (command.charAt(0) >= '0' && command.charAt(0) <= '9' || command.equals(".")) {
+                textField.setText(textField.getText() + command);
+            } else if (command.equals("=")) {
+                num2 = Double.parseDouble(textField.getText());
+                switch (operator) {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        break;
+
+                }
+                textField.setText("" + result);
+                operator = "";
+            } else {
+                if (!operator.isEmpty()) return;
+                operator = command;
+                num1 = Double.parseDouble(textField.getText());
+                textField.setText("");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Calculator::new);
+    }
+
+}
